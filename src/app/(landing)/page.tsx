7b19296e499cfc3904dd33cbdcd6ea0a4668a8bd
takeshi0518 <y.takeshi0518@gmail.com>
2026-02-01 00:03:19 +0900

    feat: usageコンポーネントの雛形を作成

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,6 +1,7 @@
 import Feature from '@/components/landing/feature';
 import Hero from '@/components/landing/hero';
 import RealScreen from '@/components/landing/real-screen';
+import Usage from '@/components/landing/usage';
 
 export default function LandingPage() {
   return (
@@ -8,6 +9,7 @@ export default function LandingPage() {
       <Hero />
       <Feature />
       <RealScreen />
+      <Usage />
     </>
   );
 }
---
+++ src/components/landing/usage.tsx
@@ -0,0 +1,12 @@
+import SectionTitle from './section-title';
+
+export default function Usage() {
+  return (
+    <div>
+      <div className="container mx-auto px-4">
+        <SectionTitle>Growth Finderの使い方</SectionTitle>
+        <div></div>
+      </div>
+    </div>
+  );
+}
