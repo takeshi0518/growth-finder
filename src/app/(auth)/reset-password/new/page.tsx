26d2a5e2cb47508c550c223f0b2c4c04f130dbb9
takeshi0518 <y.takeshi0518@gmail.com>
2026-02-06 20:39:05 +0900

    feat: 新しいパスワード入力画面を実装

---
+++ src/app/(auth)/reset-password/new/page.tsx
@@ -0,0 +1,27 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import NewPasswordForm from './new-password-form';
+
+export default function NewPsswordPage() {
+  return (
+    <Card className="w-full max-w-md">
+      <CardHeader className="text-center">
+        <CardTitle className="text-2xl md:text-3xl">
+          新しいパスワードを設定
+        </CardTitle>
+        <CardDescription>
+          ８文字以上の英数字を含むパスワードを設定してください。
+        </CardDescription>
+      </CardHeader>
+
+      <CardContent>
+        <NewPasswordForm />
+      </CardContent>
+    </Card>
+  );
+}
