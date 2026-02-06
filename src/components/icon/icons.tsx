ba01c6243d2d49c7875fd63259c628b434e78efc
takeshi0518 <y.takeshi0518@gmail.com>
2026-02-06 19:59:00 +0900

    feat: パスワード変更アイコンを追加

--- src/components/icon/icons.tsx
+++ src/components/icon/icons.tsx
@@ -7,6 +7,7 @@ import {
   ClipboardPenLine,
   MailCheck,
   ArrowLeft,
+  KeyRound,
 } from 'lucide-react';
 
 import { FaXTwitter, FaGithub } from 'react-icons/fa6';
@@ -21,6 +22,7 @@ export const Icons = {
   ClipboardPenLine,
   MailCheck,
   ArrowLeft,
+  KeyRound,
   FaXTwitter,
   FaGithub,
   FcGoogle,
