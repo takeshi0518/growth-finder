10100aab86a03dee4e6a760dfcfbc07e6ee68caa
takeshi0518 <y.takeshi0518@gmail.com>
2026-02-13 21:08:48 +0900

    feat: setup-formにuseFormを適用

--- src/app/(auth)/setup/setup-form.tsx
+++ src/app/(auth)/setup/setup-form.tsx
@@ -4,33 +4,63 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import LoaderCircleIcon from '@/components/shared/loader-circle';
+import { useState } from 'react';
+import { createClient } from '@/lib/supabase/client';
+import { useRouter } from 'next/navigation';
+import { useForm } from 'react-hook-form';
+import { SetupInput, setupSchema, SingupInput } from '@/lib/validations/auth';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 export default function SetupForm() {
+  const supabase = createClient();
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SetupInput>({
+    resolver: zodResolver(setupSchema),
+    mode: 'onChange',
+  });
+
+  async function onSubmit(data: SingupInput) {}
   return (
     <div className="space-y-6">
       <form className="space-y-4">
         {/* 名前 */}
         <div className="space-y-2">
           <Label htmlFor="name">名前</Label>
-          <Input id="name" type="text" placeholder="山田太郎" />
-          {/* {errors.name && (
+          <Input
+            id="name"
+            type="text"
+            placeholder="山田太郎"
+            {...register('name')}
+          />
+          {errors.name && (
             <p className="text-sm text-red-500">{errors.name.message}</p>
-          )} */}
+          )}
         </div>
 
         {/* 店舗名 */}
         <div className="space-y-2">
           <Label htmlFor="storeName">店舗名</Label>
-          <Input id="storeName" type="text" placeholder="◯◯◯店" />
+          <Input
+            id="storeName"
+            type="text"
+            placeholder="◯◯◯店"
+            {...register('storeName')}
+          />
 
-          {/* {errors.storeName && (
+          {errors.storeName && (
             <p className="text-sm text-red-500">{errors.storeName.message}</p>
-          )} */}
+          )}
         </div>
 
         {/* ログインボタン */}
         <Button type="submit" className="w-full" size="lg">
-          {/* {isLoading.signup ? <LoaderCircleIcon /> : 'アカウントを作成'} */}
+          {isLoading ? <LoaderCircleIcon /> : 'アカウントを作成'}
           登録する
         </Button>
       </form>
