96d2b1f8b41bae929519e1722cf8cb58249672d4
takeshi0518 <y.takeshi0518@gmail.com>
2026-02-16 15:12:30 +0900

    refactor: middlewareのセッション管理を分離   - updateSession関数を分離   - middleware専用なのでファイル名を明確化

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,38 +1,8 @@
-import { createServerClient } from '@supabase/ssr';
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse, type NextRequest } from 'next/server';
+import { updateSession } from './lib/supabase/middleware';
 
 export async function middleware(request: NextRequest) {
-  let supabaseResponse = NextResponse.next({
-    request,
-  });
-
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        getAll() {
-          return request.cookies.getAll();
-        },
-        setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value }) =>
-            request.cookies.set(name, value)
-          );
-          supabaseResponse = NextResponse.next({
-            request,
-          });
-          cookiesToSet.forEach(({ name, value, options }) =>
-            supabaseResponse.cookies.set(name, value, options)
-          );
-        },
-      },
-    }
-  );
-
-  //セッションをリフレッシュ
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
+  const { user, supabase, supabaseResponse } = await updateSession(request);
 
   const pathname = request.nextUrl.pathname;
 
@@ -66,6 +36,8 @@ export async function middleware(request: NextRequest) {
   if (isAuthPath && user) {
     return NextResponse.redirect(new URL('/admin', request.url));
   }
+
+  return supabaseResponse;
 }
 
 export const config = {
