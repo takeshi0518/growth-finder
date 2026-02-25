55bc58183d1967306553d76e59ddfc7996c90657
takeshi0518 <y.takeshi0518@gmail.com>
2026-02-25 18:47:22 +0900

    fix: プロフィール画像が登録されないエラーを解決
    - next.config.tsにremotePatternsを追加
    - protocol: 'http'
    - hostname: 'loclahost'
    - port: '54321'
    - pathname: '/storage/v1/object/public/**'

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,12 @@ const nextConfig: NextConfig = {
         pathname: '/storage/v1/object/public/**',
       },
       {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '54321',
+        pathname: '/storage/v1/object/public/**',
+      },
+      {
         protocol: 'https',
         hostname: '*.supabase.co',
         port: '',
