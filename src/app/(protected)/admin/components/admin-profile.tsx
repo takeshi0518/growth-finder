a6b23893100969039835f924f53d161ec7855646
takeshi0518 <y.takeshi0518@gmail.com>
2026-03-09 13:41:50 +0900

    feat: 管理者ダッシュボードにプロフィール情報を表示
    - page.tsxでprofilesからデータを取得しAdminProfileに渡す
    - AdminProfileにProfile型を定義しデータを表示

--- src/app/(protected)/admin/components/admin-profile.tsx
+++ src/app/(protected)/admin/components/admin-profile.tsx
@@ -1,14 +1,25 @@
+import { Tables } from '../../../../../types/supabase';
 import { Icons } from '@/components/icon/icons';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Label } from '@/components/ui/label';
+import Image from 'next/image';
 
-export default function AdminProfile() {
+type Profile = Pick<
+  Tables<'profiles'>,
+  'name' | 'store_name' | 'role' | 'email' | 'avatar_url'
+>;
+
+type AdminProfile = {
+  profile: Profile;
+};
+
+export default function AdminProfile({ profile }: AdminProfile) {
   return (
     <Card>
       <CardHeader>
         <CardTitle>
           <div className="flex items-center gap-2">
-            <Icons.Pencil className="w-5 h-5" />
+            <Icons.UserCircle className="w-5 h-5" />
             プロフィール
           </div>
         </CardTitle>
@@ -17,35 +28,35 @@ export default function AdminProfile() {
       <CardContent>
         <div className="flex flex-col sm:flex-row items-center gap-6">
           <div className="w-24 h-24 rounded-full border-2 overflow-hidden bg-card flex items-center justify-center">
-            {/* {avatarUrl ? (
-                      <Image
-                        src={avatarUrl}
-                        alt={targetStaff.name}
-                        width={100}
-                        height={100}
-                        className="object-cover"
-                      />
-                    ) : (
-                      <Icons.UserCircle className="w-16 h-16 text-muted-foreground" />
-                    )} */}
+            {profile.avatar_url ? (
+              <Image
+                src={profile.avatar_url}
+                alt={profile.name}
+                width={100}
+                height={100}
+                className="object-cover"
+              />
+            ) : (
+              <Icons.UserCircle className="w-16 h-16 text-muted-foreground" />
+            )}
           </div>
 
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-x-8 gap-y-3 text-sm">
             <div className="flex gap-2">
               <Label>店舗名</Label>
-              <p className="text-muted-foreground">GoodCafe</p>
+              <p className="text-muted-foreground">{profile.store_name}</p>
             </div>
             <div className="flex gap-2">
               <Label>役職</Label>
-              <p className="text-muted-foreground">admin</p>
+              <p className="text-muted-foreground">{profile.role}</p>
             </div>
             <div className="flex gap-2">
               <Label>名前</Label>
-              <p className="text-muted-foreground">山田太郎</p>
+              <p className="text-muted-foreground">{profile.name}</p>
             </div>
             <div className="flex gap-2">
               <Label>メールアドレス</Label>
-              <p className="text-muted-foreground">taro@gmail.com</p>
+              <p className="text-muted-foreground">{profile.email}</p>
             </div>
           </div>
         </div>
