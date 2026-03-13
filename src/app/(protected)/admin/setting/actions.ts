52d8012b1b87001c8f767f2eb469315c4583d2c6
takeshi0518 <y.takeshi0518@gmail.com>
2026-03-13 16:56:20 +0900

    refactor: admin/setting/actions.tsにrequireAdminを適用
    - userチェックをrequireAdminに変更
    - DBに登録する値をvalidatedから取得することに変更
    - updatePasswordの例外処理をthrowに変更

--- src/app/(protected)/admin/setting/actions.ts
+++ src/app/(protected)/admin/setting/actions.ts
@@ -1,6 +1,7 @@
 'use server';
 
 import { createClient } from '@/lib/supabase/server';
+import { requireAdmin } from '@/lib/utils/requireAdmin';
 import {
   UpdatePasswordInput,
   updatePasswordSchema,
@@ -11,27 +12,26 @@ import {
 export async function updateProfile(data: UpdateProfileInput) {
   const supabase = await createClient();
 
+  const { user } = await requireAdmin(supabase);
+
   const validated = updateProfileSchema.safeParse(data);
   if (!validated.success) {
     throw new Error('入力内容を確認してください');
   }
 
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-  if (!user) throw new Error('認証エラーが発生しました');
-
-  if (data.email !== user.email) {
-    const { error } = await supabase.auth.updateUser({ email: data.email });
+  if (validated.data.email !== user.email) {
+    const { error } = await supabase.auth.updateUser({
+      email: validated.data.email,
+    });
     if (error) throw new Error('メールアドレスの更新に失敗しました');
   }
 
   const { error } = await supabase
     .from('profiles')
     .update({
-      name: data.name,
-      store_name: data.storeName,
-      email: data.email,
+      name: validated.data.name,
+      store_name: validated.data.storeName,
+      email: validated.data.email,
     })
     .eq('id', user.id);
 
@@ -43,27 +43,25 @@ export async function updateProfile(data: UpdateProfileInput) {
 export async function updatePassword(data: UpdatePasswordInput) {
   const supabase = await createClient();
 
+  const { user } = await requireAdmin(supabase);
+
   const validated = updatePasswordSchema.safeParse(data);
+
   if (!validated.success) {
-    return { error: '入力内容を確認してください' };
+    throw { error: '入力内容を確認してください' };
   }
 
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-  if (!user?.email) return { error: '認証エラーが発生しました' };
-
   const { error: signInError } = await supabase.auth.signInWithPassword({
-    email: user.email,
-    password: data.currentPassword,
+    email: user.email!,
+    password: validated.data.currentPassword,
   });
   if (signInError) return { error: '現在のパスワードが正しくありません' };
 
   const { error } = await supabase.auth.updateUser({
-    password: data.password,
+    password: validated.data.password,
   });
 
-  if (error) return { error: 'パスワードの更新に失敗しました' };
+  if (error) throw { error: 'パスワードの更新に失敗しました' };
 
   return { success: true };
 }
