3ad5378ae4fc6782fbf27ac5b8a055f4f4d5768a
takeshi0518 <y.takeshi0518@gmail.com>
2026-03-21 19:48:11 +0900

    fix: ラベル削除

--- src/app/(protected)/admin/staff/[staffId]/evaluation/components/evaluation-list.tsx
+++ src/app/(protected)/admin/staff/[staffId]/evaluation/components/evaluation-list.tsx
@@ -4,10 +4,7 @@ import EvaluationItemList from './evaluation-itemList';
 export default function EvaluationList() {
   return (
     <Card className="w-full max-w-200 mx-auto">
-      <CardHeader className="flex items-center justify-around">
-        {/* <div className="text-sm bg-primary/10 rounded-xl p-2">評価項目</div>
-        <div className="text-sm bg-primary/10 rounded-xl p-2">スコア</div> */}
-      </CardHeader>
+      <CardHeader className="flex items-center justify-around"></CardHeader>
       <CardContent>
         <EvaluationItemList />
       </CardContent>
