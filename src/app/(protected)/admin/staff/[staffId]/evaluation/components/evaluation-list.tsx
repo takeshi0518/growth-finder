d4758fd8fa204f4ecc857a690bca8b979ab702c5
takeshi0518 <y.takeshi0518@gmail.com>
2026-03-21 19:44:04 +0900

    feat: 評価入力UIを実装
    - EvaluationItemコンポーネント名をEvaluationItemListコンポーネントに変更
    - EvaluationItemコンポーネントを作成
    - レイアウトの観点からEvaluationListのラベルを削除

--- src/app/(protected)/admin/staff/[staffId]/evaluation/components/evaluation-list.tsx
+++ src/app/(protected)/admin/staff/[staffId]/evaluation/components/evaluation-list.tsx
@@ -1,13 +1,16 @@
 import { Card, CardContent, CardHeader } from '@/components/ui/card';
+import EvaluationItemList from './evaluation-itemList';
 
 export default function EvaluationList() {
   return (
-    <Card>
+    <Card className="w-full max-w-200 mx-auto">
       <CardHeader className="flex items-center justify-around">
-        <div className="text-sm bg-primary/10 rounded-xl p-2">評価項目</div>
-        <div className="text-sm bg-primary/10 rounded-xl p-2">スコア</div>
+        {/* <div className="text-sm bg-primary/10 rounded-xl p-2">評価項目</div>
+        <div className="text-sm bg-primary/10 rounded-xl p-2">スコア</div> */}
       </CardHeader>
-      <CardContent>{/* Todo: evaluation-itemをループ */}</CardContent>
+      <CardContent>
+        <EvaluationItemList />
+      </CardContent>
     </Card>
   );
 }
