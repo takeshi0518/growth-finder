d445fa43fa89b0bbc72ae89d28bf660b5266251b
takeshi0518 <y.takeshi0518@gmail.com>
2026-04-24 19:09:23 +0900

    feat: スタッフプロフィールカードを実装

--- src/app/(protected)/staff/page.tsx
+++ src/app/(protected)/staff/page.tsx
@@ -1,16 +1,15 @@
+import ProfileCard from '@/components/shared/profile-card';
 import { createClient } from '@/lib/supabase/server';
 import { requireStaff } from '@/lib/utils/requireStaff';
 
 export default async function StaffPage() {
   const supabase = await createClient();
 
-  const { orgId, profile } = await requireStaff(supabase);
-
-  console.log(orgId, profile);
+  const { profile } = await requireStaff(supabase);
 
   return (
-    <div>
-      <div>Staff Page</div>
+    <div className="mt-20 max-w-7xl mx-auto w-full py-6 px-4 space-y-6">
+      <ProfileCard profile={profile} />
     </div>
   );
 }
