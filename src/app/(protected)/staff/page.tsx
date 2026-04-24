2b393380675197aa10eed2b8cd4b6605822f4928
takeshi0518 <y.takeshi0518@gmail.com>
2026-04-24 18:16:53 +0900

    feat: ログイン済みのユーザーの権限をチェックするrequireStaffを作成

--- src/app/(protected)/staff/page.tsx
+++ src/app/(protected)/staff/page.tsx
@@ -1,4 +1,13 @@
-export default function StaffPage() {
+import { createClient } from '@/lib/supabase/server';
+import { requireStaff } from '@/lib/utils/requireStaff';
+
+export default async function StaffPage() {
+  const supabase = await createClient();
+
+  const { orgId, profile } = await requireStaff(supabase);
+
+  console.log(orgId, profile);
+
   return (
     <div>
       <div>Staff Page</div>
---
+++ src/lib/utils/requireStaff.ts
@@ -0,0 +1,26 @@
+import { SupabaseClient } from '@supabase/supabase-js';
+import { Database } from '../../../types/supabase';
+
+export async function requireStaff(supabase: SupabaseClient<Database>) {
+  const {
+    data: { user },
+    error: authError,
+  } = await supabase.auth.getUser();
+  if (authError || !user) throw new Error('認証エラーが発生しました');
+
+  const { data: profile, error: profileError } = await supabase
+    .from('profiles')
+    .select('store_name, role, organization_id, email, avatar_url, name')
+    .eq('id', user.id)
+    .single();
+
+  if (profileError) throw new Error('プロフィールの取得に失敗しました');
+
+  if (!profile || profile.role !== 'staff' || !profile.organization_id) {
+    throw new Error('この操作を行う権限または、組織設定がありません');
+  }
+
+  const orgId = profile.organization_id;
+
+  return { user, profile, orgId };
+}
