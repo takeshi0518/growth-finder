02feac887bdecc5850d4abf2da14220a853b74b5
takeshi0518 <y.takeshi0518@gmail.com>
2026-04-29 15:45:59 +0900

    feat: アプリイメージ画像をHeroコンポーネントに追加
    - lgサイズ以上でテキストと画像を横並びにするように変更

--- src/app/(landing)/components/hero.tsx
+++ src/app/(landing)/components/hero.tsx
@@ -6,40 +6,44 @@ import Container from './container';
 
 export default function Hero() {
   return (
-    <section className="pt-32 pb-20">
+    <section className="min-h-screen flex items-center pt-32 lg:pt-10">
       <Container>
-        <div className="flex flex-col items-center gap-12">
-          <h1 className="font-bold text-3xl sm:text-5xl md:text-7xl">
-            Growth Finder
-          </h1>
-          <div className="text-center">
-            <p className="text-base mb-6 sm:text-2xl md:text-3xl">
-              スタッフの成長を可視化する
-              <br />
-              評価ツール
-            </p>
-            <p className="text-base sm:text-2xl md:text-3xl text-muted-foreground">
-              現場の課題から作った
-              <br />
-              手書き評価ツールをデジタル化
-            </p>
+        <div className="flex flex-col lg:flex-row items-center gap-12">
+          <div className="flex flex-col gap-8 lg:flex-1 text-center md:text-center">
+            <h1 className="font-bold text-3xl sm:text-5xl md:text-7xl">
+              Growth Finder
+            </h1>
+            <div className="text-center">
+              <p className="text-base mb-6 sm:text-2xl md:text-3xl">
+                スタッフの成長を可視化する
+                <br />
+                評価ツール
+              </p>
+              <p className="text-base sm:text-2xl md:text-3xl text-muted-foreground">
+                現場の課題から作った
+                <br />
+                手書き評価ツールをデジタル化
+              </p>
+            </div>
+            <div className="flex flex-col gap-4 md:flex-row md:justify-center sm:gap-8 md:gap-12">
+              <Button size="lg" asChild>
+                <Link href="/demo">デモで今すぐ試す</Link>
+              </Button>
+              <Button size="lg" variant="outline" asChild>
+                <Link href="/signup">無料ではじめる</Link>
+              </Button>
+            </div>
           </div>
-          <div className="flex flex-col gap-4 sm:flex-row sm:gap-8 md:gap-12">
-            <Button size="lg" asChild>
-              <Link href="/demo">デモで今すぐ試す</Link>
-            </Button>
-            <Button size="lg" variant="outline" asChild>
-              <Link href="/signup">無料ではじめる</Link>
-            </Button>
+          <div className="lg:flex-1">
+            <Image
+              src="/images/growth-finder5.png"
+              alt="Growth Finder ダッシュボード画像"
+              width={800}
+              height={500}
+              className="rounded-lg shadow-xl border"
+              priority
+            />
           </div>
-          <Image
-            src="/images/dummy.png"
-            alt="Growth Finder ダッシュボード画像"
-            width={800}
-            height={500}
-            className="rounded-lg shadow-xl"
-            priority
-          />
         </div>
       </Container>
     </section>
