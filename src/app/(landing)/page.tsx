2f113466ac85e01a979a06b8375c0ff4587a3fe1
takeshi0518 <y.takeshi0518@gmail.com>
2026-06-13 13:10:51 +0900

    feat: RealScreen, Usage, CallToActionにアニメーションを付与

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -4,15 +4,26 @@ import Footer from './components/footer';
 import Hero from './components/hero';
 import RealScreen from './components/real-screen';
 import Usage from './components/usage';
+import FadeIn from './components/fade-in';
 
 export default function LandingPage() {
   return (
     <div className="space-y-24 md:space-y-32 lg:space-y-40">
       <Hero />
+
       <ProblemSolution />
-      <RealScreen />
-      <Usage />
-      <CallToAction />
+
+      <FadeIn delay={0}>
+        <RealScreen />
+      </FadeIn>
+
+      <FadeIn delay={0}>
+        <Usage />
+      </FadeIn>
+
+      <FadeIn delay={0}>
+        <CallToAction />
+      </FadeIn>
       <Footer />
     </div>
   );
