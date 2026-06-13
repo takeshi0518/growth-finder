e5e1a58f820fabb9611f9b0d57c95416c9bebf4c
takeshi0518 <y.takeshi0518@gmail.com>
2026-06-13 12:49:27 +0900

    style: Usageコンポーネントのレイアウトを調整

--- src/app/(landing)/components/usage.tsx
+++ src/app/(landing)/components/usage.tsx
@@ -41,7 +41,7 @@ type UsageItemProps = {
 export default function Usage() {
   return (
     <section id="usage">
-      <Container>
+      <Container className="space-y-20">
         <SectionTitle>Growth Finderの使い方</SectionTitle>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           {usageList.map((item) => (
